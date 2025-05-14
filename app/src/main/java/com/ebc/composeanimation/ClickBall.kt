1c57bf40285f76d3c3e1c7520c404eef43cee826
Julio Caro <jcarocota@gmail.com>
2025-05-13 19:34:31 -0700

    Click ball vacío

--- app/src/main/java/com/ebc/composeanimation/ClickBall.kt
+++ app/src/main/java/com/ebc/composeanimation/ClickBall.kt
@@ -1,2 +1,14 @@
 package com.ebc.composeanimation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ClickBall(
+    containerWidth: Dp = 300.dp,
+    ballSize: Dp = 50.dp,
+    animationDuration: Int = 1000
+) {
+
+}
