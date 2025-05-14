61e11ebb2941a257fbed11cf602509597e1f31f8
Julio Caro <jcarocota@gmail.com>
2025-05-13 20:08:24 -0700

    Proyecto completo

--- app/src/main/java/com/ebc/composeanimation/MainActivity.kt
+++ app/src/main/java/com/ebc/composeanimation/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ebc.composeanimation.ui.theme.ComposeAnimationTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,13 +26,24 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeAnimationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MyBall(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MyBall(modifier: Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Dale click a la bola!!")
+        Spacer(Modifier.height(16.dp))
+        ClickBall(animationDuration = 500, ballSize = 25.dp)
     }
 }
 
